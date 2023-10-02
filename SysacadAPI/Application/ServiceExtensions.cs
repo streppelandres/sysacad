52e1a9267325db90b3d1be7979b41df8d9d33918
Andrés Caballero Streppel <andres.streppel@gmail.com>
2023-10-02 20:38:59 -0300

    Project init

---
+++ SysacadAPI/Application/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            services.AddAutoMapper(assembly);
+            services.AddValidatorsFromAssembly(assembly);
+
+            services.AddMediatR(assembly);
+        }
+    }
+}
