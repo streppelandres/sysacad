bee7026d47fb77f6316662e3ce2e9052429dbb6d
Andrés Caballero Streppel <andres.streppel@gmail.com>
2023-10-09 00:03:02 -0300

    Get Student schedule

---
+++ SysacadAPI/Application/Features/Student/Queries/GetStudentScheduleQuery/GetStudentScheduleQueryResponse.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOs;
+
+namespace Application.Features.Student.Queries.GetStudentScheduleQuery
+{
+    public class GetStudentScheduleQueryResponse
+    {
+        public string Name { get; set; }
+        public string Division { get; set; }
+        public List<ScheduleDto> Schedules { get; set; }
+    }
+}
